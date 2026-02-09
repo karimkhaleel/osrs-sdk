3c3be3e47e378126c99490a96f19890bafc38802
Karim Khaleel <code.karim@gmail.com>
2026-02-09 12:40:27 -0500

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowInsecurePredicate = _: true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "default";
+          buildInputs = with pkgs; [ nodejs_16 ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
